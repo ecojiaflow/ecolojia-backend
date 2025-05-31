95b94a3c47954b548baa908a2c9b9386d77747fc
Salim Benamara <salim.d.benamara@gmail.com>
2025-05-31 17:08:37 +0200

    ✅ fix prisma global typing for Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  let prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const prisma = global.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
-
