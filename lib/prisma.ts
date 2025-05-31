dbc1ede05f6b95f92fce06c093ef8bc314febbe3
Salim Benamara <salim.d.benamara@gmail.com>
2025-05-31 17:03:08 +0200

    ✅ fix eslint: replace var with let

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prisma: PrismaClient | undefined
+  let prisma: PrismaClient | undefined
 }
 
 const prisma = global.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma
+
