d80805bf8b9745c69bf89b10886a54b198b392c5
Salim Benamara <salim.d.benamara@gmail.com>
2025-05-31 16:54:00 +0200

    🐛 Fix prisma global type for Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const prisma = global.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma
