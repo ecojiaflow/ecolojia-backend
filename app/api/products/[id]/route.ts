eb1bbf5665511b9c62638f5aefdcc4d5289b6bf6
Salim Benamara <salim.d.benamara@gmail.com>
2025-06-01 15:46:39 +0200

    Initial commit from local

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,18 +1,20 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
   try {
+    const id = context.params.id;
     const body = await req.json();
 
     const updatedProduct = await prisma.product.update({
-      where: { id: params.id },
-      data: body
+      where: { id },
+      data: body,
     });
 
-    return new Response(JSON.stringify(updatedProduct), { status: 200 });
+    return new NextResponse(JSON.stringify(updatedProduct), { status: 200 });
   } catch (error) {
     console.error('❌ Erreur PUT /api/products/[id]:', error);
-    return new Response('Erreur serveur', { status: 500 });
+    return new NextResponse('Erreur serveur', { status: 500 });
   }
 }
+
